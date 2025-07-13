7fda22cde83bd1be89b9331fd3e9b2cdc20d4639
HridoyVaraby <sbi.ridoy@gmail.com>
2025-07-14 04:00:59 +0600

    feat(Footer): update copyright year to dynamic range and add developer credit
    Add dynamic year range in copyright notice and include developer attribution with link to Varabit website

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -100,7 +100,7 @@ const Footer = () => {
         </div>
 
         <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-          <p>&copy; 2024 CCID Bangladesh. All rights reserved.</p>
+          <p>&copy; 2024 - {new Date().getFullYear()} CCID Bangladesh. All rights reserved. Developed by <a href="https://varabit.com" >Varabit Web Design & Development.</a></p>
         </div>
       </div>
     </footer>
